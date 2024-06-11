7ef81b5ccd865f13f40105974915b49fd08731b4
Chiranjeevi Ganesh R B <gani1357@gmail.com>
2024-06-11 12:49:22 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2_instance" {
+resource "aws_instance" "my_ec2" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   subnet_id       = var.subnet_id
